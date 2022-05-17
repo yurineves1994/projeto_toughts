3ab96a07e2a6d525a8a51c5218dfe5c72cc878f3
Yuri Neves <yurineves1934@gmail.com>
2022-05-17 18:40:41 -0300

    subindo projeto 01 - crud revista

---
+++ db/conn.js
@@ -0,0 +1,17 @@
+const {
+  Sequelize
+} = require('sequelize')
+
+const sequelize = new Sequelize('toughts2', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+})
+
+try {
+  sequelize.authenticate()
+  console.log('Conectamos com o Sequelize!')
+} catch (error) {
+  console.error('Não foi possível conectar:', error)
+}
+
+module.exports = sequelize
